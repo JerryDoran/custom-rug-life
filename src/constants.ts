f147ad641ea29c8bbd1b73a65b8f15311c13aa23
Jerome Doran <disneyfan9184@gmail.com>
2024-04-26 15:32:20 -0400

    completed hero section

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'TechPeople Blog';
-export const SITE_DESCRIPTION = 'Articles on web development, tech, and more';
+export const SITE_TITLE = 'Custom Rug Life';
+export const SITE_DESCRIPTION = 'Custom Rug Designs for People who Love Rugs';
 export const HOME_PAGE_ARTICLE_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
