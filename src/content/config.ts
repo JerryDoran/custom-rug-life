0f282cbdef87e15043cb744512358a7a41290244
Jerome Doran <disneyfan9184@gmail.com>
2024-04-27 13:07:22 -0400

    cleaned up unused files

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,12 @@
 import { defineCollection, z } from 'astro:content';
-const blogCollection = defineCollection({
+const rugCollection = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
-    pubDate: z.date(),
-    author: z.string(),
     image: z.string().optional(),
-    tags: z.array(z.string()),
   }),
 });
 
 export const collections = {
-  blog: blogCollection,
+  rug: rugCollection,
 };
